c8b1d21c00795a41569e1efb260faeb9a690a00a
Rasto <rmirek@easytype.info>
2020-03-04 16:13:32 +0100

    Initial Commit

---
+++ FleaMarket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FleaMarket
+//
+//  Created by Rastislav Mirek on 4/3/20.
+//  Copyright © 2020 FI MU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
